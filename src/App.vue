b0ecb3eb571f981fe67ae011548b152c6042408e
TuMkoO <mr.t09@mail.ru>
2023-10-20 10:57:21 +0300

    add layout components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import GameHeader from "@/components/GameHeader.vue";
+import GameFigure from "@/components/GameFigure.vue";
+import GameWrongLetters from "@/components/GameWrongLetters.vue";
+import GameWord from "@/components/GameWord.vue";
+import GamePopup from "@/components/GamePopup.vue";
+import GameNotification from "@/components/GameNotification.vue";
+</script>
 
-<template></template>
+<template>
+  <GameHeader />
+  <div class="game-container">
+    <GameFigure />
+
+    <GameWrongLetters />
+
+    <GameWord />
+  </div>
+
+  <!-- Container for final message -->
+  <GamePopup />
+
+  <!-- Notification -->
+  <GameNotification />
+</template>
 
 <style scoped></style>
